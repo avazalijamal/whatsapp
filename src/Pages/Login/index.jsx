b7a91e0ab1e0b0f928661ffaa70937d2f373d4d4
Ilqar <bakinitygames.94@gmail.com>
2023-03-28 17:33:15 +0400

    refactoring_login_And_register_page

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./style.css";
 import { profile_2 } from "../../Assets/img";
+import { Link } from "react-router-dom";
 
-import { Button, Form, Image, Input, Typography } from "antd";
+import { Button, Col, Form, Image, Input, Layout, Row, Typography } from "antd";
 import { AiOutlineUser, AiOutlineLock } from "react-icons/ai";
+import { Content } from "antd/es/layout/layout";
 
 const Index = () => {
   const onFinish = (values) => {
@@ -13,76 +15,84 @@ const Index = () => {
     console.log("Failed:", errorInfo);
   };
   return (
-    <div className='container'>
-      <Form
-        className='login-form'
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Form.Item name='profile_png_2'>
-          <Image
-            className='image'
-            width={100}
-            height={100}
-            preview={false}
-            style={{ borderRadius: "50%" }}
-            src={profile_2}
-          />
-        </Form.Item>
-        <Form.Item
-          name='username'
-          rules={[
-            {
-              required: true,
-              message: "Please input your Username!",
-            },
-          ]}
-        >
-          <Input
-            size='large'
-            prefix={<AiOutlineUser className='prefix' />}
-            placeholder='Username'
-            bordered={false}
-            className='input'
-          />
-        </Form.Item>
-        <Form.Item
-          name='password'
-          rules={[
-            {
-              required: true,
-              message: "Please input your Password!",
-            },
-          ]}
-        >
-          <Input.Password
-            size='large'
-            prefix={<AiOutlineLock className='prefix' />}
-            type='password'
-            placeholder='Password'
-            bordered={false}
-            className='input'
-          />
-        </Form.Item>
+    <Layout className='Layout'>
+      <Content className='Content'>
+        <Row style={{ height: "100%" }} justify={"center"} align='middle'>
+          <Col xs={2} sm={4} md={6} lg={8} xl={9}></Col>
+          <Col xs={20} sm={16} md={12} lg={8} xl={6}>
+            <Form
+              className='login-form'
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+            >
+              <Form.Item name='profile_png_2'>
+                <Image
+                  className='image'
+                  width={100}
+                  height={100}
+                  preview={false}
+                  style={{ borderRadius: "50%" }}
+                  src={profile_2}
+                />
+              </Form.Item>
+              <Form.Item
+                name='username'
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Username!",
+                  },
+                ]}
+              >
+                <Input
+                  size='large'
+                  prefix={<AiOutlineUser className='prefix' />}
+                  placeholder='Username'
+                  bordered={false}
+                  className='input'
+                />
+              </Form.Item>
+              <Form.Item
+                name='password'
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Password!",
+                  },
+                ]}
+              >
+                <Input.Password
+                  size='large'
+                  prefix={<AiOutlineLock className='prefix' />}
+                  type='password'
+                  placeholder='Password'
+                  bordered={false}
+                  className='input'
+                />
+              </Form.Item>
 
-        <Form.Item style={{ textAlign: "center" }}>
-          <Button
-            size='large'
-            type='primary'
-            htmlType='submit'
-            className='login-button'
-          >
-            Log in
-          </Button>
-          <Typography.Title level={5} style={{ fontWeight: "400" }}>
-            Not registered?
-            <Button type='link' style={{ padding: "2px" }}>
-              Create an Account
-            </Button>
-          </Typography.Title>
-        </Form.Item>
-      </Form>
-    </div>
+              <Form.Item style={{ textAlign: "center" }}>
+                <Button
+                  size='large'
+                  type='primary'
+                  htmlType='submit'
+                  className='login-button'
+                >
+                  Log in
+                </Button>
+                <Typography.Title level={5} style={{ fontWeight: "400" }}>
+                  Not registered?
+                  <Link to='/chat/register' style={{ padding: "2px" }}>
+                    Create an Account
+                  </Link>
+                </Typography.Title>
+              </Form.Item>
+            </Form>
+          </Col>
+          <Col xs={2} sm={4} md={6} lg={8} xl={9}></Col>
+        </Row>
+      </Content>
+    </Layout>
   );
 };
 
--- src/Pages/Register/index.jsx
+++ src/Pages/Register/index.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./style.css";
 import { profile_2 } from "../../Assets/img";
+import { Link } from "react-router-dom";
 
-import { Button, Form, Image, Input, Typography, Upload } from "antd";
+import {
+  Button,
+  Col,
+  Form,
+  Image,
+  Input,
+  Layout,
+  Row,
+  Typography,
+  Upload,
+} from "antd";
 import { UploadOutlined } from "@ant-design/icons";
 import { AiOutlineUser, AiOutlineLock } from "react-icons/ai";
 import { MdAlternateEmail } from "react-icons/md";
+import { Content } from "antd/es/layout/layout";
 
 const Index = () => {
   const onFinish = (values) => {
@@ -16,130 +28,143 @@ const Index = () => {
   };
 
   return (
-    <div className='container-reg'>
-      <Form
-        className='register-form'
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-      >
-        <Form.Item name='profile_png_2'>
-          <Image
-            className='image'
-            width={100}
-            height={100}
-            preview={false}
-            style={{ borderRadius: "50%" }}
-            src={profile_2}
-          />
-        </Form.Item>
-
-        <Form.Item
-          name='username'
-          rules={[
-            {
-              required: true,
-              message: "Please input your Fullname!",
-            },
-          ]}
-        >
-          <Input
-            size='large'
-            prefix={<AiOutlineUser className='prefix' />}
-            placeholder='Fullname'
-            bordered={false}
-            className='input'
-          />
-        </Form.Item>
-
-        <Form.Item
-          name='email'
-          rules={[
-            {
-              required: true,
-              message: "Please input your Email!",
-            },
-          ]}
-        >
-          <Input
-            size='large'
-            prefix={<MdAlternateEmail className='prefix' />}
-            type='email'
-            placeholder='Email'
-            bordered={false}
-            className='input'
-          />
-        </Form.Item>
-
-        <Form.Item
-          name='password'
-          rules={[
-            {
-              required: true,
-              message: "Please input your Password!",
-            },
-          ]}
-        >
-          <Input.Password
-            size='large'
-            prefix={<AiOutlineLock className='prefix' />}
-            placeholder='Password'
-            bordered={false}
-            className='input'
-          />
-        </Form.Item>
-
-        <Form.Item
-          name='confirm-password'
-          rules={[
-            {
-              required: true,
-              message: "Please input your Password again!",
-            },
-          ]}
-        >
-          <Input.Password
-            size='large'
-            prefix={<AiOutlineLock className='prefix' />}
-            placeholder=' Confirm Password'
-            bordered={false}
-            className='input'
-          />
-        </Form.Item>
-
-        <Form.Item name='upload' valuePropName='fileList'>
-          <Upload name='photo' action='/upload.do' listType='picture'>
-            <Button
-              color='rgb(49, 175, 145);'
-              htmlType='button'
-              size='large'
-              type='primary'
-              className='upload-button'
-              icon={<UploadOutlined />}
+    <Layout className='Layout'>
+      <Content className='Content-reg'>
+        <Row style={{ height: "100%" }} justify={"center"} align='middle'>
+          <Col xs={2} sm={4} md={6} lg={9} xl={9}></Col>
+          <Col xs={20} sm={16} md={12} lg={6} xl={6}>
+            <Form
+              className='register-form'
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
             >
-              Upload a photo
-            </Button>
-          </Upload>
-        </Form.Item>
+              <Form.Item name='profile_png_2'>
+                <Image
+                  className='image'
+                  width={100}
+                  height={100}
+                  preview={false}
+                  style={{ borderRadius: "50%" }}
+                  src={profile_2}
+                />
+              </Form.Item>
 
-        <Form.Item style={{ textAlign: "center" }} name='submit_button'>
-          <Button
-            size='large'
-            type='primary'
-            htmlType='submit'
-            className='register-button-reg'
-          >
-            Register
-          </Button>
-        </Form.Item>
+              <Form.Item
+                name='username'
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Fullname!",
+                  },
+                ]}
+              >
+                <Input
+                  size='large'
+                  prefix={<AiOutlineUser className='prefix' />}
+                  placeholder='Fullname'
+                  bordered={false}
+                  className='input'
+                />
+              </Form.Item>
 
-        <Typography.Title level={5} style={{ fontWeight: "400" }}>
-          Allready have an account?
-          <Button type='link' style={{ padding: "2px" }}>
-            Login
-          </Button>
-        </Typography.Title>
-      </Form>
-    </div>
+              <Form.Item
+                name='email'
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Email!",
+                  },
+                ]}
+              >
+                <Input
+                  size='large'
+                  prefix={<MdAlternateEmail className='prefix' />}
+                  type='email'
+                  placeholder='Email'
+                  bordered={false}
+                  className='input'
+                />
+              </Form.Item>
+
+              <Form.Item
+                name='password'
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Password!",
+                  },
+                ]}
+              >
+                <Input.Password
+                  size='large'
+                  prefix={<AiOutlineLock className='prefix' />}
+                  placeholder='Password'
+                  bordered={false}
+                  className='input'
+                />
+              </Form.Item>
+
+              <Form.Item
+                name='confirm-password'
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your Password again!",
+                  },
+                ]}
+              >
+                <Input.Password
+                  size='large'
+                  prefix={<AiOutlineLock className='prefix' />}
+                  placeholder=' Confirm Password'
+                  bordered={false}
+                  className='input'
+                />
+              </Form.Item>
+
+              <Form.Item name='upload' valuePropName='fileList'>
+                <Upload
+                  name='photo'
+                  action='/upload.do'
+                  listType='picture'
+                  multiple={false}
+                >
+                  <Button
+                    color='rgb(49, 175, 145);'
+                    htmlType='button'
+                    size='large'
+                    type='primary'
+                    className='upload-button'
+                    icon={<UploadOutlined />}
+                  >
+                    Upload a photo
+                  </Button>
+                </Upload>
+              </Form.Item>
+
+              <Form.Item style={{ textAlign: "center" }} name='submit_button'>
+                <Button
+                  size='large'
+                  type='primary'
+                  htmlType='submit'
+                  className='register-button-reg'
+                >
+                  Register
+                </Button>
+              </Form.Item>
+
+              <Typography.Title level={5} style={{ fontWeight: "400" }}>
+                Allready have an account?
+                <Link to='/chat/login' style={{ padding: "2px" }}>
+                  Login
+                </Link>
+              </Typography.Title>
+            </Form>
+          </Col>
+          <Col xs={2} sm={4} md={6} lg={9} xl={9}></Col>
+        </Row>
+      </Content>
+    </Layout>
   );
 };
 
